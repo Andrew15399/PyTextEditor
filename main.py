2f76195ef4a3b3ecc70c318714af3de0c39367a7
Andrew15399 <walnut1539@gmail.com>
2024-11-04 12:06:38 -0800

    Improved gui, input refresh on button click, label refreshing

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ from tkinter import *  # import everything from gui package tkinter
 
 class EditText:
 
+    fileName = "list.txt"  # name of file to be written and read from
+
     def __init__(self):  # constructor method
-        file1 = open("list.txt", "r")  # opens text file to read
+        file1 = open(self.fileName, "r")  # opens text file to read
         self.items = file1.readlines()  # file1.readlines() is read and stored to items as a list
         file1.close()  # file is closed
 
-        self.formatItems = ""  # initialize empty string called formatItems
+        self.formatItems = ""  # initialize empty string called formatItems to make label format and modification easier
         for item in self.items:  # for each item in items add item to listItems
             self.formatItems = self.formatItems + item
 
     def write_text(self):  # method that reads input into text file
         task = addE.get()  # gets input from addE entry widget
+        addE.delete(0, END)  # removes text from addE input field
         task += "\n"  # adds new line for file formatting
         self.items.append(task)  # task is appended to items list
-        file1 = open("list.txt", "w")  # opens text file for overwriting
+        file1 = open(self.fileName, "w")  # opens text file for overwriting or makes one if it doesn't exist
         file1.writelines(self.items)  # overwrites current text with modified items list
         file1.close()  # closes file
+        self.formatItems = self.formatItems + self.items[len(self.items) - 1]
+        listLabel.config(text="TODO LIST:\n\n" + et1.formatItems)
 
     def read_text(self):  # method that reads items in text file to console
         for item in self.items:  # for each item in items print item
@@ -26,12 +31,16 @@ class EditText:
             print(item)  # prints item to console
 
     def remove_text(self):  # method that will remove specific lines of text from text file
-        num = int(delE.get())  # sets num to int cast input from delE widget
-        num -= 1  # subtract one so the row number equals index value
+        num = int(delE.get()) - 1  # sets num to int cast input(minus one to match index) from delE widget
+        delE.delete(0, END)  # removes text from delE input field
         self.items.pop(num)  # remove item at num index
-        file1 = open("list.txt", "w")  # opens text file for overwriting
+        file1 = open(self.fileName, "w")  # opens text file for overwriting
         file1.writelines(self.items)  # overwrites current text with modified items list
         file1.close()  # closes file
+        self.formatItems = ""  # clears formatItems to be updated to remove deleted items
+        for item in self.items:  # for each item in items add item to listItems
+            self.formatItems = self.formatItems + item
+        listLabel.config(text="TODO LIST:\n\n" + et1.formatItems)
 
 
 '''Variables and Objects'''
@@ -41,39 +50,21 @@ et1 = EditText()  # obj edit text 1(et1) created
 
 '''tkinter gui setup'''
 listLabel = Label(root, text="TODO LIST:\n\n" + et1.formatItems)  # creates listLabel label widget that displays formatItems
-addLabel = Label(root, text="Add items here:")
-addE = Entry(root, width=30)  # creates addE input widget with width of 24
+addLabel = Label(root, text="Add Items Here:")  # instruction label with text "Add items here:"
 addButton = Button(root, text="Add Item", command=et1.write_text, width=10)  # creates addButton button widget with text "Add Items"
+addE = Entry(root, width=24)  # creates addE input widget with width of 24
+delLabel = Label(root, text="Input Task Number:")  # instruction label with text "Input Task Number:"
 delButton = Button(root, text="Delete Item", command=et1.remove_text, width=10)  #
-delE = Entry(root, width=30)  # creates delE input widget with width of 24
+delE = Entry(root, width=24)  # creates delE input widget with width of 24
 
 '''tkinter gui '''
 listLabel.grid(row=0, column=0, columnspan=2)
-addButton.grid(row=1, column=1)
-addE.grid(row=1, column=0)
-delButton.grid(row=2, column=1)
-delE.grid(row=2, column=0)
+addLabel.grid(row=1, column=0)
+addButton.grid(row=2, column=1)
+addE.grid(row=2, column=0)
+delLabel.grid(row=3, column=0)
+delButton.grid(row=4, column=1)
+delE.grid(row=4, column=0)
 
 
 root.mainloop()
-
-
-'''
-ans = '0'  # input answer char value created and initialized
-
-while ans != 'q' and ans != 'Q':  # creates infinite loop until q/Q is submitted
-    ans = input("A. Add Item to List, B. To Remove Item from List, C. to View List, Q. to Quit")  # sets answer to
-    # keyboard input
-
-    if ans == 'a' or ans == 'A':  # if answer value equals a/A call et1.writeText
-        et1.write_text()
-
-    elif ans == 'b' or ans == 'B':  # if answer value equals b/B call et1.removeText
-        et1.remove_text()
-
-    elif ans == 'c' or ans == 'C':  # if answer value equals c/C call et1.readText
-        et1.read_text()
-
-    elif ans == 'q' or ans == 'Q':  # if answer value equals q/Q print by and end while loop
-        print("bye for now")'''
-
